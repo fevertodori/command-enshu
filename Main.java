12c024b346271bb0f08557608c02805cf091ccb3
todori <s.todori.sys22@morijyobi.ac.jp>
2022-11-08 10:49:59 +0900

    変更したゼ！

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         
-        System.out.println("BMIを計算して肥満度を表示しようと思う。");
+        for(int i =0;i<2;i++){
+	System.out.println("BMIを計算して肥満度を表示しようと思う。");
         Scanner scan = new Scanner(System.in);
             System.out.print("・身長(m)を入力しろ：");
                 double height =  scan.nextDouble();
@@ -11,7 +12,7 @@ public class Main{
 
                 double bmi = weight / Math.pow(height, 2);
                 System.out.println("貴様のBMIは"+String.format("%.2f",bmi));
-
+	}
     }
 }
 
